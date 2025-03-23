05240517f1d0e221ec306cb5fcf421082cf80920
kishanAnthwal <kishananthwal@gmail.com>
2025-03-24 00:02:01 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const dotenv = require('dotenv');
+//const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const cors = require('cors');
 
@@ -9,8 +9,17 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+    {
+        origin:["https://frontend-nu-flame-31.vercel.app"],
+        methods:["POST","GET"],
+        credentials:true
+    }
+));
 
+app.get("/", (req, res) => {
+    res.send("Welcome to the IPL 2025 Market API!");
+});
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/clicked-data', require('./routes/clickedData'));
 const PORT = process.env.PORT || 5000;
