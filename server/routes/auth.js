86a11b108d21425877deb29349d284f6416bb3e8
kishanAnthwal <kishananthwal@gmail.com>
2025-03-23 23:56:26 +0530

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/user.js');
 
 const router = express.Router();
 
@@ -15,7 +15,7 @@ router.post('/register', async (req, res) => {
     user = new User({ username, email, password });
     await user.save();
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const token = jwt.sign({ id: user._id },'6ee7a412294365aa43e0b5fe6383da99be789398722555dfc445c17f5a6c2822bc030ae7cd18bf173dc8e817f7d3229b46ff4cac1fb12b35e15e8306a279be2f', { expiresIn: '1h' });
     res.status(201).json({ token, user });
   } catch (err) {
     res.status(500).json({ message: 'Server error' });
@@ -32,7 +32,7 @@ router.post('/login', async (req, res) => {
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ message: 'Invalid password' });
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const token = jwt.sign({ id: user._id },'6ee7a412294365aa43e0b5fe6383da99be789398722555dfc445c17f5a6c2822bc030ae7cd18bf173dc8e817f7d3229b46ff4cac1fb12b35e15e8306a279be2f', { expiresIn: '1h' });
     res.json({ token, user });
   } catch (err) {
     res.status(500).json({ message: 'Server error' });
