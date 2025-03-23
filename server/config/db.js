6831af6879323d5c6d233ca7d19b14d85335c10b
kishanAnthwal <kishananthwal@gmail.com>
2025-03-23 23:49:42 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect('mongodb+srv://kishananthwal:blogPassword@blogcluster.e6f2p.mongodb.net/blogs?retryWrites=true&w=majority&appName=blogCluster');
     console.log('✅ MongoDB Connected...');
   } catch (err) {
     console.error('❌ MongoDB Connection Failed:',err.message);
