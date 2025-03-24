e98d2f3d8517578c70081305f7d7b8fc81c5d2b1
kishanAnthwal <kishananthwal@gmail.com>
2025-03-24 15:02:23 +0530

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('../models/user.js');
+const User = require('../models/User');
 
 const router = express.Router();
-const JWT_SECRET ='6ee7a412294365aa43e0b5fe6383da99be789398722555dfc445c17f5a6c2822bc030ae7cd18bf173dc8e817f7d3229b46ff4cac1fb12b35e15e8306a279be2f';
+
 // Register user
 router.post('/register', async (req, res) => {
   const { username, email, password } = req.body;
@@ -12,17 +12,12 @@ router.post('/register', async (req, res) => {
     let user = await User.findOne({ email });
     if (user) return res.status(400).json({ message: 'User already exists' });
 
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-
-    user = new User({ username, email, password:hashedPassword });
+    user = new User({ username, email, password });
     await user.save();
 
-    const token = jwt.sign({ id: user._id },Jwt_SECRET , { expiresIn: '1h' });
+    const token = jwt.sign({ id: user._id },'6ee7a412294365aa43e0b5fe6383da99be789398722555dfc445c17f5a6c2822bc030ae7cd18bf173dc8e817f7d3229b46ff4cac1fb12b35e15e8306a279be2f', { expiresIn: '1h' });
     res.status(201).json({ token, user });
   } catch (err) {
-    console.error(err);
     res.status(500).json({ message: 'Server error' });
   }
 });
@@ -37,10 +32,9 @@ router.post('/login', async (req, res) => {
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ message: 'Invalid password' });
 
-    const token = jwt.sign({ id: user._id },Jwt_SECRET, { expiresIn: '1h' });
+    const token = jwt.sign({ id: user._id },'6ee7a412294365aa43e0b5fe6383da99be789398722555dfc445c17f5a6c2822bc030ae7cd18bf173dc8e817f7d3229b46ff4cac1fb12b35e15e8306a279be2f', { expiresIn: '1h' });
     res.json({ token, user });
   } catch (err) {
-     console.error(err);
     res.status(500).json({ message: 'Server error' });
   }
 });
