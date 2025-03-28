e7c356b851bec99d4ea9287f0525aadf3874b7c6
kishanAnthwal <kishananthwal@gmail.com>
2025-03-28 22:12:43 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -42,5 +42,5 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
-const PORT =6000;
+const PORT =8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
