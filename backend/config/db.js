924023fb1a0500c0ef01fd854f572642c07885cf
kishanAnthwal <kishananthwal@gmail.com>
2025-03-30 13:29:04 +0530

    new Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://kishananthwal:blogPassword@blogcluster.e6f2p.mongodb.net/blogs?retryWrites=true&w=majority&appName=blogCluster');
-    console.log('✅ MongoDB Connected...');
+   const conn = await mongoose.connect("mongodb+srv://kishananthwal:blogPassword@blogcluster.e6f2p.mongodb.net/?retryWrites=true&w=majority&appName=blogCluster");
+   if(conn){
+    console.log('✅ MongoDB Connected...')
+   }
   } catch (err) {
     console.error('❌ MongoDB Connection Failed:',err.message);
     process.exit(1);
